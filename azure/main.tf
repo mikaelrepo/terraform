df37f8c4a9ff125e0c30431bd003f904dcdaab89
Mikael Hemma <mikael.hemma@gmail.com>
2022-11-16 13:00:27 +0100

    update variables

--- azure/main.tf
+++ azure/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   features {}
-}
-
-locals {
-  resource_name = "${var.application_name}-${var.environment_name}-${var.country_code}"
 }
 
 resource "azurerm_resource_group" "rg" {
--- azure/variables.tf
+++ azure/variables.tf
@@ -5,3 +5,23 @@ variable "location" {
   description = "The name of the Azure location"
   default     = "West Europe"
 }
+
+variable "subscription_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
